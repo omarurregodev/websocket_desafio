75481ea5563368100339102f6596517b8b57738b
omarurregodev <omarurrego07@gmail.com>
2022-10-03 17:13:42 -0500

    add produ

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+const socket = io.connect();
+
+socket.on("productos", (data) => {
+    showProductos(data)
+})
+
+function showProductos(data) {
+    const htmlProductos = data.map((prod) => {
+        return `
+        <tr><td>${prod.nombre}</td><td>$ ${prod.precio}</td><td><img src="${prod.url}" width="50" alt="not found"></td></tr>
+        `
+    })
+    document.getElementById("tableBodyProd").innerHTML = htmlProductos;
+}
+
+function addProduct(e) {
+    const producto = {
+        nombre: document.getElementById("nombre").value,
+        precio: document.getElementById("precio").value,
+        url: document.getElementById("foto").value
+    };
+    socket.emit("new-product", producto);
+    return false;
+}
+
+socket.on("messages", (data) => {
+    render(data)
+})
+
+function render(data) {
+    const html = data.map((element) => {
+        return `
+        <div>
+            <strong>${element.author}: </strong>
+            <em>${element.text}</em>
+        </div>
+        `
+    }).join(" ");
+
+    document.getElementById("mensajes").innerHTML = html;
+}
+
+function addMessages(e) {
+    const mensaje = {
+        author: document.getElementById("userName").value,
+        text: document.getElementById("texto").value
+    }
+    socket.emit("new-message", mensaje);
+    return false;
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Productos = require('./api/productos.js');
+const PORT = 8080;
 const { Server: HttpServer } = require("http");
 const { Server: IOServer } = require("socket.io");
 
@@ -7,12 +7,18 @@ const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
 
-const PORT = 8080;
-const server = httpServer.listen(PORT, () => {
-    console.log("Servidor levantado en el puerto " + server.address().port);
+app.use(express.static("./public"));
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: __dirname })
 });
 
-server.on("error", (error) => console.log(`Hubo un error ${error}`));
+const productos = [
+    {
+        nombre: "Primer prod",
+        precio: 234.54,
+        url: "www.google.co"
+    }
+];
 
 const messages = [
     {
@@ -33,61 +39,19 @@ const messages = [
 io.on("connection", (socket) => {
     console.log("Se conectó un usuario!");
 
-    socket.emit("messages", messages);
+    socket.emit("productos", productos);
 
-    socket.on("new-message", (data) => {
-        messages.push(data);
-        io.sockets.emit("messages", messages)
+    socket.on("new-product", (data) => {
+        productos.push(data);
+        io.sockets.emit("productos", productos)
     })
+
+    // socket.on("new-message", (data) => {
+    //     messages.push(data);
+    //     io.sockets.emit("messages", messages)
+    // })
 }) 
 
-let productos = new Productos();
-
-// configuración engine
-
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
-const router = express.Router();
-app.use("/api", router);
-
-router.use(express.urlencoded({ extended: false }))
-router.use(express.json());
-
-router.get("/productos/listar", (req, res) => {
-    res.json(productos.listarAll())
-})
-
-router.get("/productos/listar/:id", (req, res) => {
-    let { id } = req.params;
-    res.json(productos.listar(id));
-})
-
-router.post("/productos/guardar", (req, res) => {
-    let producto = req.body;
-    productos.guardar(producto);
-    res.redirect("/");
-})
-
-router.put("/productos/actualizar/:id", (req, res) => {
-    let { id } = req.params;
-    let producto = req.body;
-    productos.actualizar(producto, id);
-    res.json(producto)
-})
-
-router.delete("/productos/borrar/:id", (req, res) => {
-    let { id } = req.params;
-    let producto = productos.borrar(id);
-    res.json(producto);
-})
-
-router.get("/productos/vista", (req, res) => {
-    let prods = productos.listarAll();
-
-    res.render("layouts/index", {
-        productos: prods,
-        hayProductos: prods.length
-    })
-})
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log("Server On")
+});    
